2b8023d387404b62d3aee5279310535dc25cda02
mansi103 <36470888+mansi103@users.noreply.github.com>
2019-03-19 10:25:16 +0530

    Update SimpleCompoundImplementation.java

--- src/main/java/com/mansi/simplecompound/services/SimpleCompoundImplementation.java
+++ src/main/java/com/mansi/simplecompound/services/SimpleCompoundImplementation.java
@@ -1,19 +1,24 @@
 package com.mansi.simplecompound.services;
 
 public class SimpleCompoundImplementation implements SimpleCompoundServices {
+	/**
+	* Simple interest Formula = (P*R*T)/100
+	*/
 	@Override
 	public double simpleinterest(double principle, double rate, int time) {
-		// TODO Auto-generated method stub
 		double simpleinterestresult = 0.0;
 		simpleinterestresult = (principle * rate * time) / 100.0;
 		return simpleinterestresult;
 	}
-
+	/**
+	* Compound interest formula = P * Math.pow((1 + r/n),nt) - P
+	*/
 	@Override
 	public double compoundinterest(double principle, double rate, int n) {
-		// TODO Auto-generated method stub
+		double amountresult = 0.0;
 		double compoundinterestresult = 0.0;
-		compoundinterestresult = principle * Math.pow((1 + rate / 100.0), n);
+		amountresult = principle * Math.pow((1 + rate / 100.0), n);
+		compoundinterestresult = amountresult - principle;
 		return compoundinterestresult;
 	}
 
